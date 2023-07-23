bb53175c574b61d3e5077cc5578584ec111a13d0
diana <moyseeko@gmail.com>
2023-07-23 18:16:26 +0300

    completed task 2

---
+++ task 2/Program.cs
@@ -0,0 +1,13 @@
+﻿// задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*
+Console.Write("imput a first number' ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("imput a second number' ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+
+if(num1 > max) max = num1;
+if(num2 > max) max = num2;
+
+Console.WriteLine(max);
+*/
