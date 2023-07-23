b2172e57e98130eb4c8a9a9a6a42c32cf5807e47
diana <moyseeko@gmail.com>
2023-07-23 19:05:30 +0300

    completed task 6

---
+++ task6/Program.cs
@@ -0,0 +1,14 @@
+﻿// задача 6:Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("imput a number' ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No!");
+}
