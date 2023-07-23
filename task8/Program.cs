05a1bea5ee2bec4399f2228a2a0bbc9fe021878e
diana <moyseeko@gmail.com>
2023-07-23 19:25:36 +0300

    completed task 8

---
+++ task8/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N
+
+Console.Write("imput a number' ");
+int numInt = Convert.ToInt32(Console.ReadLine());
+
+int count =1;
+
+while(count <= numInt)
+{
+    if(count % 2 ==0 )
+    Console.Write(count + ", ");
+    count++;
+}
